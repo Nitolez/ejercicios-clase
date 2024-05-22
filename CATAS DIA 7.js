54fb7fa80133bfeae6f89194962e3381501c6b7b
Nitolez <antonioglezt98@gmail.com>
2024-05-22 16:18:27 +0200

    asd

---
+++ CATAS DIA 7.js
@@ -0,0 +1,35 @@
+//CATA 1
+const regExp = /^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{4,19}$/;
+function validPass(password){
+  if(regExp.test(password)){
+    return "VALID"
+  } else {return "INVALID"}
+}
+/* ^: Inicio de la cadena.
+(?=.*[a-zA-Z]): Asegura que haya al menos una letra.
+(?=.*\d): Asegura que haya al menos un número.
+[a-zA-Z\d]{4,19}: Asegura que la cadena tenga entre 4 y 19 caracteres alfanuméricos.
+$: Fin de la cadena.*/
+
+//CATA 2
+
+const regExp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/;
+
+function password(str) {
+  if(regExp.test(str)){
+    return true
+    } else {
+      return false
+    }
+}
+
+/* ^: Inicio de la cadena.
+(?=.*[a-z]): Asegura que haya al menos una letra minúscula.
+(?=.*[A-Z]): Asegura que haya al menos una letra mayúscula.
+(?=.*\d): Asegura que haya al menos un número.
+.{8,}: Asegura que la cadena tenga al menos 8 caracteres alfanuméricos.
+$: Fin de la cadena.*/
+
+
+//CATA 3
+
